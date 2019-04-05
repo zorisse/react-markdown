4ef2a4b5aa4a35d4467fb04a16e68108a61022dd
Nouri T <zorisse@gmail.com>
2019-04-05 13:49:54 +0200

    fini

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import marked from 'marked';
+import { sampleText } from './sampleText';
 import './App.css';
 
+
 class App extends Component {
+  // state
+  state = {
+    text: sampleText
+  }
+  // methods life cycle 
+  componentDidMount = () => {
+    console.log('je viens de monter ')
+    const text = localStorage.getItem('text')
+    if (text !== "") {
+      this.setState({
+        text
+      })
+    } else {
+      this.setState({
+        text: sampleText
+      })
+    }
+
+  }
+  componentDidUpdate = () => {
+    const { text } = this.state
+    console.log('update happenned')
+    localStorage.setItem('text', text)
+  }
+  // methods 
+  handleChange = (event) => {
+    //1 copier le state 
+    let text = { ...this.state.text }
+    //2 changer et store les valeurs 
+    let changedText = event.target.value
+    text = changedText
+    //setState 
+    this.setState({
+      text: text
+    })
+  }
+  renderText = (txt) => {
+    const __html = marked(txt, { sanitize: true });
+    return { __html }
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="container">
+        <div className="row">
+
+          <div className="col-sm-6">
+            <textarea
+              className="form-control"
+              rows="35"
+              onChange={this.handleChange}
+              value={this.state.text}
+            >
+
+            </textarea>
+          </div>
+          <div className="col-sm-6">
+            <div dangerouslySetInnerHTML={this.renderText(this.state.text)} >
+
+
+            </div>
+          </div>
+
+        </div>
+      </div >
     );
   }
 }
